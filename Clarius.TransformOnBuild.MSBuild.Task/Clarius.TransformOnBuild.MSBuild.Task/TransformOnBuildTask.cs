7be30efaa58ca0fd606baac014ed684ed50e2e0b
Michael Naumov <mnaoumov@gmail.com>
2015-06-20 01:20:08 +0300

    Create custom MSBuild task to cover TransformOnBuildTask

---
+++ Clarius.TransformOnBuild.MSBuild.Task/Clarius.TransformOnBuild.MSBuild.Task/TransformOnBuildTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clarius.TransformOnBuild.MSBuild.Task
+{
+    public class TransformOnBuildTask : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
