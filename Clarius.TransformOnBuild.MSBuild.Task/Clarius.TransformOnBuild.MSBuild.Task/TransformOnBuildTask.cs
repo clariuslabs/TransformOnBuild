c7892cb040514c6125d2d3a21ea1b9823736612b
Michael Naumov <mnaoumov@gmail.com>
2015-06-20 03:17:20 +0300

    Remove using

--- Clarius.TransformOnBuild.MSBuild.Task/Clarius.TransformOnBuild.MSBuild.Task/TransformOnBuildTask.cs
+++ Clarius.TransformOnBuild.MSBuild.Task/Clarius.TransformOnBuild.MSBuild.Task/TransformOnBuildTask.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
