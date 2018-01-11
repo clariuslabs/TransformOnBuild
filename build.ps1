01de292bf13f598143b7eb9d0de4ff122eeda542
Michael Naumov <michael.naumov@volia-software.com>
2018-01-11 20:39:51 +0200

    Rewrite build to use PowerShell instead of MSBuild

--- build.ps1
+++ build.ps1
@@ -13,4 +13,57 @@ $script:ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 trap { throw $Error[0] }
 
-& 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe' "$PSScriptRoot\build.proj" /verbosity:normal /nr:false /p:PackageVersion=$PackageVersion /p:AutoPush=$($AutoPush.ToString().ToLower())
+function Main
+{
+    . "$PSScriptRoot\Exec.ps1"
+
+    CompileSolution
+    DownloadNuGet
+    PackNuGetPackage
+    PublishNuGetPackage
+}
+
+function CompileSolution
+{
+    $msbuildPath = 'c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'
+    $solutionPath = "$PSScriptRoot\Clarius.TransformOnBuild.MSBuild.Task\Clarius.TransformOnBuild.MSBuild.Task.sln"
+
+    exec { & $msbuildPath $solutionPath }
+}
+
+function DownloadNuGet
+{
+    $nugetPath = "$PSScriptRoot\.nuget\nuget.exe"
+    $nuGetExeUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
+    if (-not (Test-Path -Path $nugetPath)
+    {
+        New-Item -Path ($nugetPath | Split-Path) -ItemType Directory -Force
+        Invoke-WebRequest -Uri $nuGetExeUrl -OutFile $nugetPath
+    }
+}
+
+function PackNuGetPackage
+{
+    $nugetPath = "$PSScriptRoot\.nuget\nuget.exe"
+    $nuspecPath = "$PSScriptRoot\nuget\Clarius.TransformOnBuild.nuspec"
+    $dropPath = "$PSScriptRoot\drop"
+
+    exec { & $nugetPath pack -NoPackageAnalysis $nuspecPath -Version $PackageVersion -OutputDirectory $dropPath }
+}
+
+function PublishNuGetPackage
+{
+    if (-not $AutoPush)
+    {
+        return
+    }
+
+    $nugetPath = "$PSScriptRoot\.nuget\nuget.exe"
+    $dropPath = "$PSScriptRoot\drop"
+    $nugetRepositoryUrl = 'https://www.nuget.org/api/v2/package'
+
+    exec { & $nugetPath push $dropPath -Source $nugetRepositoryUrl }
+}
+
+Main
